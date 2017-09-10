d855d49b5673a823d12890762bce062b65b12992
Pablo Daniel Bobadilla <danielbobadillaarg@gmail.com>
2017-09-10 12:19:38 +0100

    primero

---
+++ Calculo Interes Compuesto/ConsolaCalculoInteresCompuesto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsolaCalculoInteresCompuesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capital = 10000;
+
+            double tasa_de_interes_diaria = 0.0074;
+
+            int n;
+
+            for (n = 1; n < 730; n++)
+            {
+                capital = capital + (capital * tasa_de_interes_diaria);
+            }
+
+
+            Console.WriteLine(capital.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
