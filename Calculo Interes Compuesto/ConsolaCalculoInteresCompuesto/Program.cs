5495ad13f111ded4ec39def7f1346ae55abe0fd6
DanielBoba <danielbobadillaarg@gmail.com>
2017-09-23 17:41:46 +0200

    785

--- Calculo Interes Compuesto/ConsolaCalculoInteresCompuesto/Program.cs
+++ Calculo Interes Compuesto/ConsolaCalculoInteresCompuesto/Program.cs
@@ -6,13 +6,13 @@ namespace ConsolaCalculoInteresCompuesto
     {
         static void Main(string[] args)
         {
-            double capital = 10000;
+            double capital = 1;
 
-            double tasa_de_interes_diaria = 0.0074;
+            double tasa_de_interes_diaria = 0.007;
 
             int n;
 
-            for (n = 1; n < 730; n++)
+            for (n = 1; n < 365 ; n++)
             {
                 capital = capital + (capital * tasa_de_interes_diaria);
             }
